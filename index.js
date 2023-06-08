ce65f46e115d20f370deaa869c45582046fec8aa
Iftekher Aziz <azizchy02@gmail.com>
2023-06-09 02:02:16 +0600

    POST user data into MongoDB

--- index.js
+++ index.js
@@ -26,6 +26,62 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Connect database collection:
+        const userCollection = client.db("maxcoach").collection("users");
+
+
+        /* Users Related API's */   
+        // 1.GET users data from MongoDB:
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        // 2.POST users data on MongoDB:
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'User already exists' })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        /* // 3.GET user if admin: 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        });
+
+        // 4.GET user if instructor:
+        app.get('/users/instructor/:email',  async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ instructor: false })
+            }
+
+            const query = { email: email }
+            const user = await userCollection.findOne(query);
+            const result = { instructor: user?.role === 'instructor' }
+            res.send(result);
+        }) */
+
+    
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
